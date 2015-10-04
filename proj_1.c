be1738b3fb885a5c89777fb75666487170441c49
aeomijie <aeomijie@gmail.com>
2015-10-03 18:53:13 -0700

    added functions to q.h and created queue and node structures

--- proj_1.c
+++ proj_1.c
@@ -1,1 +1,18 @@
-//to test q.h
+/**************************************************************************
+                        Arizona State University
+                        CSE430 Operating Systems
+                                Project #1
+
+                Abigael Omijie | abigael.omijie@asu.edu
+                Katie Gosse    |
+**************************************************************************/
+
+#include "q.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+   queue q;
+
+   return 0;
+}
--- q.h
+++ q.h
@@ -1,1 +1,44 @@
-//where functions defined will be
+/**************************************************************************
+                        Arizona State University
+                        CSE430 Operating Systems
+                                Project #1
+
+                Abigael Omijie | abigael.omijie@asu.edu
+                Katie Gosse    |
+**************************************************************************/
+
+#ifndef Q_H
+#define Q_H
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct node{
+   int x; //payload 
+   struct node *prev;
+   struct node *next;
+}node;
+
+typedef struct queue{
+   node *head; //oldest element in the queue
+   node *tail; //newest element in the queue
+   int currSize; //current size of the queue
+   int maxSize; //max size of the queue
+}queue;
+
+/*Constructor: creates an empty array queue pointed to variable head*/
+//InitQueue(&head);
+
+/*returns a pointer to a new q-element*/
+//NewItem();
+
+/*adds a queue item pointed to by item, to queue pointed to by head*/
+//AddQueue(&head, item);
+
+/*deletes an item from head and returns a pointer to the deleted item*/
+//DelQueue(&head);
+
+/*moves the header pointer to the next element in the queue*/
+//RotateQ(&head);
+
+
+#endif
