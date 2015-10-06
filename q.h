487d903c18416d06d0487c94febce5ccad13ba17
aeomijie <aeomijie@gmail.com>
2015-10-05 22:43:55 -0700

    Wrote InitQueue, addQueue, and started delQueue also included isEmpty function

--- q.h
+++ q.h
@@ -12,8 +12,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef enum { false, true } bool ;
+
 typedef struct node{
-   int x; //payload 
+   int payload; //data 
    struct node *prev;
    struct node *next;
 }node;
@@ -24,7 +26,6 @@ typedef struct queue{
    int currSize; //current size of the queue
    int maxSize; //max size of the queue
 }queue;
-
 
 /*Constructor: creates an empty array queue pointed to variable head*/
 void InitQueue(queue *q){
@@ -37,13 +38,61 @@ void InitQueue(queue *q){
 }
 
 /*returns a pointer to a new q-element*/
-//NewItem();
+node* NewItem(){
+   node* item = (node*) malloc(sizeof(node)); //allocate memory for a node 
+   item->payload = 0;
+   item->next = NULL;
+   item->prev = NULL;
+
+   return item;    
+}
+
+//checks to see if the queue is empty
+bool isEmpty(queue *q){
+   if(q != NULL && q->head != NULL)
+      return false;
+   else
+      return true;
+}
 
 /*adds a queue item pointed to by item, to queue pointed to by head*/
-//AddQueue(&head, item);
+void AddQueue(queue *q, node* item){
+   if(q != NULL && item != NULL){
+      if(!isEmpty(q)){
+        if(q->currSize == q->maxSize){ //when the q is full
+  	   q->head->payload = item->payload;
+	   q->head = q->head->next;
+	}
+	else{ //non-empty queue with room available
+	   item->prev = q->tail;
+           item->next = q->head;
+           q->tail->next = item;
+           q->head->prev = item;
+           q->tail = item;
+	} 
+      }
+      else{ //if the queue is empty
+	   item->prev = NULL;
+	   item->next = NULL;
+	   q->head = item;
+	   q->tail = item;
+      }
+   }
+} 
 
 /*deletes an item from head and returns a pointer to the deleted item*/
-//DelQueue(&head);
+DelQueue(queue *q){
+   if(!isEmpty(q)){
+      if(currSize == 1){ //if queue has only one item
+         free (item);
+	 q->head = NULL;
+	 q->tail = NULL;
+      }
+      else{ //if queue has more than one item
+         
+       }
+   }
+}
 
 /*moves the header pointer to the next element in the queue*/
 //RotateQ(&head);
