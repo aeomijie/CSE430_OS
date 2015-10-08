fefb863bd4c4de5fa9c3cf6f0f8c0d58130c0470
aeomijie <aeomijie@gmail.com>
2015-10-07 18:41:07 -0700

    began writing run and yield functions

--- TCB.h
+++ TCB.h
@@ -1,7 +1,7 @@
 /**************************************************************************
                         Arizona State University
                         CSE430 Operating Systems
-                                Project #1
+                                Project #2
 
                 Abigael Omijie | abigael.omijie@asu.edu
                 Katie Gosse    | kgosse@asu.edu
@@ -21,16 +21,11 @@ typedef struct TCB_t {
 
 /*init_TCB: initialize TCB structure elements for a new thread*/
 void init_TCB (TCB_t *tcb, void *function, void *stackP, int stack_size){
-   /*clear tcb memory by setting to null*/
-   memset(tcb, '\0', sizeof(TCB_t));
-   /*set a parent context*/
-   getcontext(&tcb->context);
-   /*set the context's thread stack pointer*/
-   tcb->context.uc_stack.ss_sp = stackP;
-   /*set the context's stack size*/
-   tcb->context.uc_stack.ss_size = (size_t) stack_size;
-   /*modify the context with the new user context parameters*/
-   makecontext(&tcb->context, function, 0);
+   memset(tcb, '\0', sizeof(TCB_t)); //clear tcb memory by setting to null
+   getcontext(&tcb->context); //set a parent context
+   tcb->context.uc_stack.ss_sp = stackP; //set the context's thread stack pointer
+   tcb->context.uc_stack.ss_size = (size_t) stack_size; //set the context's stack size
+   makecontext(&tcb->context, function, 0); //modify the context with the new user context parameters
 }
 
 #endif /*TCB_H*/
