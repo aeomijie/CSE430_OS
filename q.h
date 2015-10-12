ea2cd802b625b68bdeb0094a9fa43ba090399be1
kgosse412 <kgosse@asu.edu>
2015-10-12 00:50:08 -0700

    Replaced node structure with TCB_t. Step 1 of Project 2.

--- q.h
+++ q.h
@@ -16,13 +16,14 @@
 
 typedef enum { false, true } bool ;
 
-typedef struct node{
+// Need to use TCB_t, instead of node
+/*typedef struct node{
    int payload; //element data
    struct node *prev, *next; //ptrs for each element
-}node_t;
+}node_t;*/
 
 typedef struct queue{
-   node_t* head; //head of the queue
+   TCB_t* head; //head of the queue
    int currSize; //current number of items in queue
    int maxSize; //max number of items in queue
 }queue_t;
@@ -37,20 +38,21 @@ void InitQueue(queue_t *q){
    }
 }
 
+//Is this function needed now???
 /*Returns a pointer to a new q-element*/
-node_t* NewItem(){
+/*TCB_t* NewItem(){
    static int i = 0;
-   node_t* item = (node_t*)malloc(sizeof(node_t)); //allocate memory for a an element 
+   TCB_t* item = (TCB_t*)malloc(sizeof(TCB_t)); //allocate memory for a an element 
    item->payload = i;
    item->next = NULL;
    item->prev = NULL;
    ++i;
 
    return item;
-}
+}*/
 
 /*Adds a queue item pointed to by item, to queue pointed to by head*/
-bool AddQueue(queue_t *q, node_t* item){
+bool AddQueue(queue_t *q, TCB_t* item){
    /*Both q and the item exist and the queue isn't full*/
    if(q != NULL && item != NULL && q->currSize != q->maxSize){
       if(q->head == NULL){
@@ -72,15 +74,15 @@ bool AddQueue(queue_t *q, node_t* item){
    return false;
 }
  
-void FreeItem(node_t* item){
+void FreeItem(TCB_t* item){
    free(item);
 }
 
 /*Deletes an item from head and returns a pointer to the deleted item*/
-node_t* DelQueue(queue_t *q){
+TCB_t* DelQueue(queue_t *q){
    /*A queue with elements must exist to delete from*/
    if(q->head != NULL){
-      node_t* item;
+      TCB_t* item;
       /*If head is the only element in queue*/
       if(q->head->next == q->head && q->head->prev == q->head){
          item = q->head;
