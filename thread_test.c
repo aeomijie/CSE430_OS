d8feeeb37163d754a59baa8d2f687ff77d2e8b7b
kgosse412 <kgosse@asu.edu>
2015-10-12 00:54:01 -0700

    Wrote test functions and main.

--- thread_test.c
+++ thread_test.c
@@ -9,8 +9,43 @@
 
 #include "threads.h"
 
-int main(int argc, char argv[]){
+/*Write  a driver program in a file called thread_test.c. Into thread_test.c.c include threads.h which includes q.h, which includes TCB.h, which includes ucontext.h.
+Declare a global RunQ.
+Write a few functions with infinite loops (put a yield in each loop). Note: try to write thread functions that are meaningful, use global and local variables
+In main, initialize RunQ, start threads using the functions you defined using start_thread.
+Call run() and watch.
+Call friends and family*/
 
+queue_t *RunQ;
+static int global = 0;
+
+void *func1();
+void *func2();
+
+int main(int argc, char argv[]){
+   //initialize RunQ
+   InitQueue(RunQ);
+   
+   //Start threads
+   start_thread(func1());
+   start_thread(func2());
+   
+   //call run from threads
+   run();
 
    return 0;
+}
+
+void *func1(){
+   static int local = 0;
+   printf("Function one - local: %d, global: %d\n", local, global);
+   ++local;
+   ++global;
+}
+
+void *func2(){
+   static int local = 0;
+   printf("Function two - local: %d, global: %d\n", local, global);
+   ++local;
+   ++global;
 }
