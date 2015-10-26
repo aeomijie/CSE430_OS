a5a280d5dc49d60ace7c4991de56692442dfdbbc
unknown <eaomijie@eaomijie-MOBL1.amr.corp.intel.com>
2015-10-26 14:18:27 -0700

    Added files: sem.h and proj_3.c

---
+++ proj_3.c
@@ -0,0 +1,10 @@
+/**************************************************************************
+			Arizona State University
+		        CSE430: Operation Systems
+			     Project #3
+		 Abigael Omijie | abigael.omijie@asu.edu
+		 Katie Gosse    | kgosse@asu.edu
+**************************************************************************/
+
+#include "threads.h"
+#include "sem.h"
