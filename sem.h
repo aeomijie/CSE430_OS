cf2b8062a71216900a6894e35accff54ac161108
unknown <eaomijie@eaomijie-MOBL1.amr.corp.intel.com>
2015-10-26 14:13:56 -0700

    Added files: sem.h and proj_3.c

---
+++ sem.h
@@ -0,0 +1,28 @@
+/**************************************************************************
+                        Arizona State University
+            		
+			CSE430 Operating Systems
+
+			      Project #3
+
+		Abigael Omijie | abigael.omijie@asu.edu
+										                Katie Gosse    | kgosse@asu.edu
+										***************************************************************************/
+										#ifndef SEM_H									#define SEM_H
+
+#include "threads.h"
+
+//Semaphore data structure
+typedef struct Semaphore_t {
+    
+
+} Sempahore_t;
+
+//Initializes the value field with the specified value
+void InitSem(sem, value){
+
+}
+
+										
+										
+										#endif //SEM_H
