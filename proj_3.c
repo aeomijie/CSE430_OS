7a9c35ae911e7a536da7a8a40ba1a68a8d3e67de
kgosse412 <kgosse@asu.edu>
2015-11-08 20:29:35 -0700

    Wrote main, producer, consumer, putItem, and getItem, and fixed a couple sem.h errors.

--- proj_3.c
+++ proj_3.c
@@ -8,15 +8,68 @@
 
 #include "threads.h"
 #include "sem.h"
+#include <stdlib.h> // For rand()
 
+#define BUFF_SZ 4 // Buffer size
+Sem_t *full, *empty;
+int buffer[BUFF_SZ];
+//int item = 0;
+int in = 0, out = 0;
 
+void producer();
+void consumer();
+void putItem(int item);
+int getItem();
+
+int main (int argc, char argv[]){
+   // Initialize semephores full and empty
+   InitSem(&full, 0);
+   InitSem(&empty, BUFF_SZ);
+
+   // Start producer and consumer threads
+   start_thread(&producer);
+   start_thread(&producer);
+   start_thread(&consumer);
+   start_thread(&consumer);
+
+   // And run
+   run();
+
+   return 0;
+}
 
 //producer
 void producer(){
-
+   int item;
+   while (1){
+      item = (rand() % 100) + 1; // Random num b/t 1 and 100
+      printf("Producer created item %d\n", item);
+      putItem(item);
+      sleep(1);
+   }
 }
 
 //consumer
 void consumer(){
+   int item;
+   while (1){
+      item = getItem();
+      printf("Consumer used item %d\n", item);
+      sleep(1);
+   }
+}
 
+void putItem(int item){
+   P(&empty);
+   buffer[in] = item;
+   in = (in + 1) % BUFF_SZ;
+   V(&full);
+}
+
+int getItem(){
+   P(&full);
+   int temp = buffer[out];
+   out = (out + 1) % BUFF_SZ;
+   V(&empty);
+   return temp;
 }
--- sem.h
+++ sem.h
@@ -11,7 +11,7 @@
 #include "threads.h"
 
 //Semaphore data structure
-typedef struct Sem_t {
+typedef struct Sem{
    int count;
    TCB_t *queue;
 
@@ -19,27 +19,28 @@ typedef struct Sem_t {
 
 //Initializes the value field with the specified value
 void InitSem(Sem_t **S, int value){
-   *S = malloc(1, sizeof(Sem_t));
+   *S = malloc(sizeof(Sem_t));
    (*S)->count = value;
    InitQueue(&((*S)->queue));
 }
 
 //Decrements a sempahores count and waits enter the RunQ
 void P(Sem_t **S){
-   *S->count--;
+   (*S)->count--;
    if((*S)->count < 0){
        TCB_t *currProcess = DelQueue(&RunQ);
-       AddQueue(&((*S)->queue) , &currentProcess);
-       swapcontext(&(currProcess->context) , &(RunQ->context))
+       AddQueue(&((*S)->queue) , currProcess);
+       swapcontext(&(currProcess->context) , &(RunQ->context));
+   }
 }
 
 //Increments a semaphores count, wakes up the next waiting semaphore,
 //and yields to the next process
 void V(Sem_t **S){
-   *S->count++;
+   (*S)->count++;
    if((*S)->count <= 0){
       TCB_t *blockedProcess = DelQueue(&((*S)->queue));
-      AddQueue(&RunQ, &blockedProcess);
+      AddQueue(&RunQ, blockedProcess);
    }
    yield();
 }
